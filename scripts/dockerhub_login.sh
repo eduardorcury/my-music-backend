a5424f1c79c9ea0b1bba6050d6a5b72fbd351399
eduardorcury <eduardoribeirocury@gmail.com>
2022-11-26 22:14:59 -0400

    echo user

--- scripts/dockerhub_login.sh
+++ scripts/dockerhub_login.sh
@@ -1,5 +1,5 @@
 username=$(aws ssm get-parameters --region sa-east-1 --names dockerhub_username --with-decryption)
 password=$(aws ssm get-parameters --region sa-east-1 --names dockerhub_password --with-decryption)
-echo username
-echo password
+echo "$username"
+echo "$password"
 docker login --username username --password password
