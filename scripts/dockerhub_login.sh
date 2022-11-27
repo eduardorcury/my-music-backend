fdefa886744b9aaa89da31d98dbf238505f5a031
eduardorcury <eduardoribeirocury@gmail.com>
2022-11-27 10:34:08 -0400

    corrigindo busca de parametro

--- scripts/dockerhub_login.sh
+++ scripts/dockerhub_login.sh
@@ -1,5 +1,5 @@
-username=$(aws ssm get-parameters --region sa-east-1 --name dockerhub_username --with-decryption --query "Parameter.Value")
-password=$(aws ssm get-parameters --region sa-east-1 --name dockerhub_password --with-decryption --query "Parameter.Value")
+username=$(aws ssm get-parameters --region sa-east-1 --name dockerhub_username --with-decryption --query "Parameters[0].Value")
+password=$(aws ssm get-parameters --region sa-east-1 --name dockerhub_password --with-decryption --query "Parameters[0].Value")
 echo "$username"
 echo "$password"
 docker login --username username --password password
