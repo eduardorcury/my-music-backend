bce1fa97ca5ff08d61f41e020e92ad285d625407
eduardorcury <eduardoribeirocury@gmail.com>
2022-12-03 19:50:57 -0400

    corrigindo login docker

--- scripts/dockerhub_login.sh
+++ scripts/dockerhub_login.sh
@@ -1,5 +1,2 @@
-username=$(aws ssm get-parameters --region sa-east-1 --name dockerhub_username --with-decryption --query "Parameters[0].Value")
-password=$(aws ssm get-parameters --region sa-east-1 --name dockerhub_password --with-decryption --query "Parameters[0].Value")
-echo "$username"
-echo "$password"
-docker login --username "$username" --password "$password"
+username=$(aws ssm get-parameters --region sa-east-1 --name dockerhub_username --with-decryption --query "Parameters[0].Value" --output text)
+aws ssm get-parameters --region sa-east-1 --name dockerhub_password --with-decryption --query "Parameters[0].Value" --output text | docker login --username $username --password-stdin
