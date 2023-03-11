3297144f19d1065f9c2ba314ef8f83bce6b0125d
eduardorcury <eduardoribeirocury@gmail.com>
2023-03-11 12:38:17 -0400

    adicionando blocking

--- src/main/kotlin/com/erc/clients/spotify/autorizacao/AutorizadorClient.kt
+++ src/main/kotlin/com/erc/clients/spotify/autorizacao/AutorizadorClient.kt
@@ -1,5 +1,6 @@
 package com.erc.clients.spotify.autorizacao
 
+import io.micronaut.core.annotation.Blocking
 import io.micronaut.http.HttpHeaders.AUTHORIZATION
 import io.micronaut.http.HttpResponse
 import io.micronaut.http.MediaType.APPLICATION_FORM_URLENCODED
@@ -14,6 +15,7 @@ import io.micronaut.http.client.annotation.Client
 interface AutorizadorClient {
 
     @Post(value = "/api/token")
+    @Blocking
     @Consumes(APPLICATION_JSON)
     @Produces(APPLICATION_FORM_URLENCODED)
     fun gerarToken(grant_type: String? = "client_credentials",
